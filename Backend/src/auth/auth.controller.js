fa15c48518ad3c9583d5c08bec482a0de2f041d1
Randy <randycast21@gmail.com>
2026-05-05 23:01:21 -0600

    Método Refresh de Access Token; Primera Version del Modulo Auth completa

--- Backend/src/auth/auth.controller.js
+++ Backend/src/auth/auth.controller.js
@@ -1,7 +1,18 @@
-import { registerUser, registerSchema } from "./auth.service.js";
+import {
+  registerUser,
+  loginUser,
+  registerSchema,
+  loginSchema,
+  refreshTokenService,
+} from "./auth.service.js";
 import { formatZodError } from "../utils/zodErrorFormatter.js";
 import { ZodError } from "zod";
 
+///////////////
+// HTTP Controllers
+///////////////
+
+// Registro de usuario
 export const register = async (req, res) => {
   try {
     const data = registerSchema.parse(req.body);
@@ -12,9 +23,7 @@ export const register = async (req, res) => {
       message: "Usuario creado",
       user,
     });
-
   } catch (error) {
-
     // 👇 ERROR DE ZOD
     if (error instanceof ZodError) {
       return res.status(400).json({
@@ -27,5 +36,41 @@ export const register = async (req, res) => {
     res.status(400).json({
       error: error.message,
     });
+  }
+};
+
+// Login de usuario
+export const login = async (req, res) => {
+  try {
+    const data = loginSchema.parse(req.body);
+
+    const result = await loginUser(data);
+
+    res.status(200).json(result);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        error: "Validation error",
+        details: formatZodError(error),
+      });
+    }
+
+    // 👇 ERROR NORMAL (login, DB, etc)
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+// Refresh token
+export const refresh = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
+
+    const result = await refreshTokenService(refreshToken);
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
   }
 };
--- Backend/src/auth/auth.service.js
+++ Backend/src/auth/auth.service.js
@@ -1,6 +1,16 @@
 import bcrypt from "bcrypt";
 import { z } from "zod";
-import { findUserByEmail, createUser } from "./auth.reposPrisma.js";
+import jwt from "jsonwebtoken";
+
+import {
+  findUserByEmail,
+  findUserById,
+  createUser,
+} from "./auth.reposPrisma.js";
+
+//////////////////
+// CRUD Auth
+//////////////////
 
 // Registro de usuario
 export const registerUser = async (data) => {
@@ -21,15 +31,116 @@ export const registerUser = async (data) => {
     username,
     email,
     passwordHash: hashedPassword,
-    is_active: true
+    is_active: true,
   });
 
   return user;
 };
 
-// Schema de validación (contrato de entrada)
+// Login User
+export const loginUser = async (data) => {
+  const { email, password } = data;
+
+  // 1. buscar usuario
+  const user = await findUserByEmail(email);
+
+  if (!user) {
+    throw new Error("Usuario no existe");
+  }
+
+  // 2. comparar password
+  const isValid = await bcrypt.compare(password, user.password_hash);
+
+  if (!isValid) {
+    throw new Error("Password incorrecto");
+  }
+
+  // 3. aquí se genera el JWT; Access para esta vez, refresh para la duracion
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  return {
+    accessToken,
+    refreshToken,
+    user: {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+  };
+};
+
+//////////////////
+// Validaciones
+//////////////////
+
+//Esquema de Regristro
 export const registerSchema = z.object({
   username: z.string().min(2),
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+//////////////////
+// JWT
+//////////////////
+
+// Creacion Access Token (Corto Plazo - Instancia Corta)
+export const generateAccessToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    },
+  );
+};
+
+//  Creacion Refresh Token (Largo Plazo - Instancia Larga)
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.REFRESH_SECRET,
+    {
+      expiresIn: "15d",
+    },
+  );
+};
+
+// Validacion de Refresh Token y Generacion de nuevo Access Token
+export const refreshTokenService = async (refreshToken) => {
+  if (!refreshToken) {
+    throw new Error("Refresh token requerido");
+  }
+
+  try {
+    // 1. validar refresh token
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+
+    // 2. buscar usuario
+    const user = await findUserById(decoded.id);
+
+    if (!user) {
+      throw new Error("Usuario no existe");
+    }
+
+    // 3. generar nuevo access token
+    const newAccessToken = generateAccessToken(user);
+
+    return {
+      accessToken: newAccessToken,
+    };
+  } catch (err) {
+    throw new Error("Refresh token inválido o expirado");
+  }
+};
